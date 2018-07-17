ea97675c4cdf3ec14f99d0118c9586277212c4b9
Hyunho <khh971228@gmail.com>
2018-07-17 14:19:12 +0900

    1. file upload , ajax로도 구현 . 덜완성됨

---
+++ src/main/java/com/quest/service/FileService.java
@@ -0,0 +1,10 @@
+package com.quest.service;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+	public String saveFile(String path, MultipartFile file) throws IllegalStateException, IOException;
+	
+}
